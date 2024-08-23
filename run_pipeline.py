34431fe16ee95e67d7928c41abf9cb5ee1b67803
subhadip978 <subhadipdas175@gmail.com>
2024-08-23 10:47:36 +0530

    modify pipelines

--- run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from pipelines.training_pipeline import train_pipeline
-from urllib.parse import urlparse
-import mlflow
-if __name__=="__main__":
-		
-	print("Tracking URI:", urlparse(mlflow.get_tracking_uri()).scheme)
-	train_pipeline(data_path="./data/train.csv")
-
-# 
